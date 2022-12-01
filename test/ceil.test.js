9d657f1540533dc408e70f0623f4f1e52781d5e6
Aamos Riihinen <aamos.riihinen@tuni.fi>
2022-12-01 19:43:59 +0000

    Add mocha command to test script, add tests for ceil

--- test/ceil.test.js
+++ test/ceil.test.js
@@ -3,7 +3,14 @@ import ceil from "../src/ceil.js"
 
 
 describe("Ceil", () => {
- it("rounds up to next integer", () =>{
-     chai.expect(ceil(3.0004)).to.equal(4)
+ it("rounds up to next integer by default", () =>{
+    chai.expect(ceil(3.0004)).to.equal(4)
+ });
+ it("rounds up to requested decimal precision", () =>{
+    chai.expect(ceil(12.123456, 3)).to.equal(12.124)
+ });
+ it("rounds up to requested integer precision", ()=>{
+    chai.expect(ceil(98765, -2)).to.equal(98800)
+    chai.expect(ceil(123, -3)).to.equal(1000)
  });
 }) 
