1599e9d4e6a0118b32add5bad90bf05f73dc0e79
Juho Nuottimäki <juho.nuottimaki@tuni.fi>
2022-12-07 11:46:01 +0200

    implement tests for add

---
+++ test/add.test.js
@@ -0,0 +1,19 @@
+import chai from "chai";
+import add from "../src/add.js";
+const expect = chai.expect;
+
+describe("Add", () => {
+    it("adds two integers", () => {
+        expect(add(12,24)).to.equal(36);
+    });
+    it("adds two floats", () => {
+        expect(add(4.5, 6.2)).to.equal(10.7);
+    });
+    it("adds negative numbers", () => {
+        expect(add(10,-2)).to.equal(8);
+    });
+    it("throws exception with NaN input", () => {
+        expect(add("football", 2022)).to.throw(Error);
+    });
+
+});
